40379e4bd0d0d38c218c3ef1e5a2f38ed3d6a1c9
Felipe Leres <leresfelipe@gmail.com>
2026-06-03 20:19:18 -0300

    Ajustando credencial

--- src/main/java/com/felipeleres/customermanagement/CustomermanagementApplication.java
+++ src/main/java/com/felipeleres/customermanagement/CustomermanagementApplication.java
@@ -1,14 +1,26 @@
 package com.felipeleres.customermanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class CustomermanagementApplication {
+public class CustomermanagementApplication implements CommandLineRunner {
 
+
+	@Autowired
+	PasswordEncoder pass;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomermanagementApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(pass.matches("Ffuh-941%@54546","$2a$10$FO3uZLz8cpaSy5xY58Z/d.0vM4lNbmkDsnsCa9LwxckdsPBS7tnuu"));
+
+	}
 }
